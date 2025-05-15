1ff61f22fa0b4d8556d7e0a8139f20f7d1aa9ce0
4Sighteducation <admin@vespa.academy>
2025-05-15 13:23:38 +0100

    setdebug to false

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const DEBUG_MODE = true; // Set to false for production or when you want to hide logs
+export const DEBUG_MODE = false; // Set to false for production or when you want to hide logs
 // You can add other global configuration flags here in the future if needed. 
