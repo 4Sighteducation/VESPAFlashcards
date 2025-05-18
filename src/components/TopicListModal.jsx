2a290428ecf5d047023e88d52695a71bd7aa0714
4Sighteducation <admin@vespa.academy>
2025-05-18 16:41:12 +0100

    New Buttons View7

--- src/components/TopicListModal.jsx
+++ src/components/TopicListModal.jsx
@@ -112,11 +112,13 @@ const TopicListModal = ({
       </div>
 
       {showTopicSlideshow && (
-        <SlideshowModal
-          cards={slideshowData.cards}
-          title={slideshowData.title}
-          onClose={() => setShowTopicSlideshow(false)}
-        />
+        <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: 1070, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <SlideshowModal
+            cards={slideshowData.cards}
+            title={slideshowData.title}
+            onClose={() => setShowTopicSlideshow(false)}
+          />
+        </div>
       )}
     </>
   );
